865cf49ac86b8f521b94d9073d26fa13ce1f8d22
young-0320 <amzoy1234@naver.com>
2026-03-20 21:55:42 +0900

    add : adder_subtractor_3bit

---
+++ projects/adder_subtractor_3bit/rtl/adder_subtractor_3bit.v
@@ -0,0 +1,9 @@
+`timescale 1ns / 1ps
+
+module adder_subtractor_3bit (
+    
+);
+
+genvar i;
+
+endmodule
---
+++ projects/adder_subtractor_3bit/rtl/full_adder.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+module full_adder (
+    input  a,
+    b,
+    c_in,
+    output c_out,
+    sum
+);
+assign sum = a ^ b ^ c_in;
+assign c_out = (a & b) | (b & c_in) | (a & c_in); 
+endmodule
+
+
+
