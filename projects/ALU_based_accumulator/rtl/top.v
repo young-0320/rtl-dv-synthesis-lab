fb93a34e6f2d38b89de2c5cca92407fb3f2c6915
Young <amzoy1234@naver.com>
2026-03-28 21:07:13 +0900

    feat: debouncer

---
+++ projects/ALU_based_accumulator/rtl/top.v
@@ -0,0 +1,37 @@
+
+module top (
+    input clk,
+    input [2:0] a,
+    input b,
+    input [1:0] f,
+    input go_btn,
+    input reset,
+    output signed [4:0] r,
+    output over
+);
+
+wire go_level;
+wire go_pulse;
+
+debouncer #(
+    .COUNT_MAX(1000000)
+) u_go_btn (
+    .clk(clk),
+    .reset(reset),
+    .btn_in(go_btn),
+    .btn_level(go_level),
+    .btn_pulse(go_pulse)
+);
+
+alu_based_accumulator u_acc (
+    .clk(clk),
+    .a(a),
+    .b(b),
+    .f(f),
+    .go_pulse(go_pulse),
+    .reset(reset),
+    .r(r),
+    .over(over)
+);
+
+endmodule
